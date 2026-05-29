d7c7f89eb0fa372b6b5285bdae8a7efacdd1ff37
Jose Ignacio Bolanos Rodrigiez <nacho3_6@hotmail.com>
2026-05-29 16:33:17 -0600

    Ejercicios de Los 4 Pilares de OOP

---
+++ Python Intermedio/Ejercicios de Los 4 Pilares de OOP/BankAccount.py
@@ -0,0 +1,28 @@
+class BankAccount():
+    def __init__(self, balance):
+        self.balance = balance
+
+    def _addBalance(self, amount):
+        self.balance+= amount
+
+    def substract_balance(self, amount):
+        current_balance = self.balance- amount
+        return current_balance
+
+class SavingsAccount(BankAccount):
+    def __init__(self, min_balance, balance):
+        self.min_balance = min_balance 
+        self.balance = balance
+
+    def substract_balance(self, amount):
+        temp_balance= self.balance - amount
+        if temp_balance < self.min_balance:
+            print('Su saldo no es suficiente para retrirar el dinero deseado')
+        else:
+            current_balance = BankAccount.substract_balance(self,amount)
+            print(f'Retire su dinero. Saldo actual es de: {current_balance}')
+
+
+my_SavingsAccount = SavingsAccount(2000, 5000)
+
+my_SavingsAccount.substract_balance(2000)
